d7aea0246b19046047118b6127c82fedf9e1d31b
Bahaaib <bahaasco@gmail.com>
2019-09-16 01:29:03 +0200

    Initial commit

---
+++ lib/home_page/home_page_viewmodel.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:bizzt_app/home_page/home_page.dart';
+
+abstract class HomePageViewModel extends State<HomePage> {}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:bizzt_app/home_page/home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  final Widget _home = HomePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: _home,
+        routes: <String, WidgetBuilder>{
+          '/home': (BuildContext context) => HomePage(),
+        });
+  }
+}
