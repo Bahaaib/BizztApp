ac06d5fba82a44fb40c0e11c8cc2a99cdc92b24a
Bahaaib <bahaasco@gmail.com>
2019-09-16 03:05:07 +0200

    created fab menu widget

--- lib/home_page/home_page_view.dart
+++ lib/home_page/home_page_view.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:bizzt_app/home_page/home_page_viewmodel.dart';
+import 'package:bizzt_app/shared_widgets/fab_menu.dart';
 
-class HomePageView extends HomePageViewModel{
+class HomePageView extends HomePageViewModel {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    return Scaffold(
+      body: Row(
+        children: <Widget>[
+          Expanded(
+              child: FabMenu()
+                  .setLocation(MenuLocation.BottomRight)
+                  .addCenterFab(Icons.menu)
+                  .addActionFab(Icons.info, onPressed: () {
+            print('Clicked info icon');
+          }).addActionFab(Icons.check_circle, onPressed: () {
+            print('Clicked check icon');
+          }))
+        ],
+      ),
+    );
   }
 }
