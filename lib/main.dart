b29bf87dd55ff8148e36806829fd95c92e897dcd
Bahaaib <bahaasco@gmail.com>
2019-09-26 13:48:18 +0200

    added stats page route to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:bizzt_app/home_page/home_page.dart';
 import 'package:bizzt_app/price_page/price_page.dart';
+import 'package:bizzt_app/stats_page/stats_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -17,6 +19,7 @@ class MyApp extends StatelessWidget {
         routes: <String, WidgetBuilder>{
           '/home': (BuildContext context) => HomePage(),
           '/price': (BuildContext context) => PricePage(),
+          '/stats': (BuildContext context) => StatsPage()
         });
   }
 }
